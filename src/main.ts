0c3c9544be87f446ea788e3f1fade83c8ed39659
Bridget <84099228+fcxzb4@users.noreply.github.com>
2026-02-12 21:49:37 -0300

    v0.1.10
    Ajuste link do front by cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://front-end-not-chan.vercel.app/',
   });
 
   await app.listen(3000);
