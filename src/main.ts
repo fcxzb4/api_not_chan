d6cbe2c245e78a8cc5b561c8bc9948e097094bdb
Bridget <84099228+fcxzb4@users.noreply.github.com>
2026-02-12 21:54:48 -0300

    v0.1.12
    Ajuste cors global

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-  origin: [
-    'https://front-end-not-chan.vercel.app'
-  ],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-});
+    origin: true,
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
-  
