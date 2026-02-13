546398852c3285033852426e18b591032357c5fb
Bridget <84099228+fcxzb4@users.noreply.github.com>
2026-02-12 21:58:33 -0300

    v0.1.13
    Ajsute de cors de novo

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: true,
-    credentials: true,
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: true,
+      methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+      allowedHeaders: ['Content-Type','Authorization'],
+      credentials: true,
+    },
   });
 
   await app.listen(3000);
