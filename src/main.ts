bb8cb3fe52199136af2ce7f80e191caee3cb4263
Bridget <84099228+fcxzb4@users.noreply.github.com>
2026-02-12 21:53:10 -0300

    v.0.9.11
    ajuste cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://front-end-not-chan.vercel.app/',
-  });
-
+  origin: [
+    'https://front-end-not-chan.vercel.app'
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+});
   await app.listen(3000);
 }
 bootstrap();
