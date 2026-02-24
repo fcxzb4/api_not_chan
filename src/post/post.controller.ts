a9ba032d8fe4e2f1c781932c84f1811d91b57f84
Bridget <84099228+fcxzb4@users.noreply.github.com>
2026-02-24 20:39:53 -0300

    v0.2.1

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpCode, HttpStatus,UnauthorizedException,Headers } from '@nestjs/common';
 import { PostsService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 
@@ -15,4 +15,18 @@ export class PostsController {
   create(@Body() createPostDto: CreatePostDto) {
     return this.postsService.create(createPostDto);
   }
+
+ @Post('webhook')
+  async handleWebhook(
+    @Body() data: any,
+    @Headers('x-api-key') apiKey: string // Captura o cabeçalho específico
+  ) {
+    // Validação de Segurança Simples
+    if (apiKey !== 'uma-senha-secreta-123') {
+      throw new UnauthorizedException('Chave de API inválida');
+    }
+
+      console.log('Dados validados e recebidos:', data);
+      return this.postsService.create(data);
+    }
 }
