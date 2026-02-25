bb349ced5fb10fc2300df042c773ff8abadca30d
Bridget <84099228+fcxzb4@users.noreply.github.com>
2026-02-25 14:21:17 -0300

    v.0.2.1.3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,9 +4,9 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  
+@Get('/health')
+getHealth() {
+  return { status: 'ok', message: 'Servidor respondendo!' };
+}
 }
