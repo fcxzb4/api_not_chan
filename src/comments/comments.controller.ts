17bc275390d28a33820d80c3c5c0adc3b57a88b3
Bridget <84099228+fcxzb4@users.noreply.github.com>
2026-03-04 20:49:56 -0300

    v.0.3.6

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -9,19 +9,15 @@ export class CommentsController {
   /**
    * Atende a: GET /posts/:postId/comments
    */
-  @Get(':postId/comments')
-  findByPost(@Param('postId') postId: string) {
-    // Usando Number(postId) pois o seu service espera um número
-    return this.commentsService.findByPost(Number(postId));
-  }
-
-  /**
-   * Atende a: POST /posts/comments (ou ajuste conforme seu DTO)
-   * Se o seu Front-end envia para /comments, use @Controller('comments') e remova o 'posts' acima.
-   * Mas para manter o padrão REST:
-   */
+ 
   @Post('comments')
   create(@Body() createCommentDto: CreateCommentDto) {
     return this.commentsService.create(createCommentDto);
+  }
+
+   @Get(':postId/comments')
+  findByPost(@Param('postId') postId: string) {
+    // Usando Number(postId) pois o seu service espera um número
+    return this.commentsService.findByPost(Number(postId));
   }
 }
