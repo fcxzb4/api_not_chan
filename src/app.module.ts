813ccb102689e9226162cd62a683171cc9f960bb
Bridget <84099228+fcxzb4@users.noreply.github.com>
2026-03-06 21:45:22 -0300

    v.0.3.7

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { PostsModule } from './post/post.module';
 import { CommentsModule } from './comments/comments.module';
 import {PrismaModule} from './prisma/prisma.module'
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PostsModule, CommentsModule , PrismaModule , AuthModule],
+  imports: [PostsModule, CommentsModule , PrismaModule , AuthModule ,ConfigModule.forRoot({ isGlobal: true })],
 })
 export class AppModule {}
